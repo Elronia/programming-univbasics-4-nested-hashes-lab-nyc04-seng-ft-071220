b5ad853f50772343413aa88d8b698edc47324b04
Ekaterina Zarudnaya <zar.catherine@gmail.com>
2020-07-20 21:12:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,7 +89,6 @@ end
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable.
   # return the entire updated hash
-  programmer_hash[:alans_new_info] = [:alan_kay][:known_for]["GUI"]
 	programmer_hash =
  		{
       :grace_hopper => {
@@ -105,13 +104,13 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    programmer_hash[:alans_new_info] = [:alan_kay][:known_for]["GUI"]
 programmer_hash
 end
 
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
   # return the entire updated hash
-  programmer_hash[:dennis_ritchie][:languges] = "Assembly"
 	programmer_hash =
  		{
       :grace_hopper => {
@@ -127,5 +126,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languges] = "Assembly"
 programmer_hash
 end
